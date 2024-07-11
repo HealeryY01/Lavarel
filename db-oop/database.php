eba5e3349e0ced1d8cc37cdd22afe50b8b1d5aae
Healer <nguyenthehieua2001@gmail.com>
2024-07-11 22:11:15 +0700

    third code

--- db-oop/database.php
+++ db-oop/database.php
@@ -7,6 +7,7 @@ class DB
     {
         $this->connection();
     }
+    //Connection
     function connection()
     {
         $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -35,8 +36,9 @@ class DB
             $list_field[] = "`{$k}`";
             $list_value[] = "'{$this->escape_string($v)}'";
         }
-        $list_field = implode(`,`, $list_field);
+        $list_field = implode(',', $list_field);
         $list_value = implode(',', $list_value);
+
         $sql = "INSERT INTO {$table} ({$list_field}) VALUES ({$list_value})";
 
 
@@ -46,12 +48,78 @@ class DB
             echo "Lỗi:" . $this->conn->error;
         }
     }
+    //Select
+    //SELECT column1.column2,...
+    //FROM table_name WHERE
+    function get($table, $field = array(), $where = "")
+    {
+        $field = !empty($field) ? implode(',', $field) : "*";
+        $where = !empty($where) ? "WHERE {$where}" : "";
+
+        $sql = "SELECT {$field} FROM {$table} {$where}";
+        echo $sql;
+        $result = $this->query($sql);
+        if ($result->num_rows > 0) {
+            $data = array();
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+            if (count($data) > 1)
+                return $data;
+            return $data[0];
+        } else {
+            echo "Không tìm thấy bản ghi nào ";
+        }
+    }
+    //Update
+    //UPDATE table_name
+    //SET column1 = value1, column2=value2, ....
+    //WHERE condition;
+    function update($table, $data = array(), $where = "")
+    {
+        $data_insert = array();
+        foreach ($data as $k => $v) {
+            $data_insert[] = "`{$k}`='{$v}'";
+        }
+        $data_insert = implode(',', $data_insert);
+        $where = !empty($where) ? "WHERE {$where}" : "";
+
+        $sql = "UPDATE {$table} SET {$data_insert} {$where}";
+        if ($this->query($sql) == TRUE) {
+            echo "Cập nhật thành công";
+        } else {
+            echo "Lỗi:" . $this->conn->error;
+        }
+    }
+    //DELETE
+    //DELETE FROM table_name WHERE condition;
+    function delete($table, $where = "")
+    {
+        $where = !empty($where) ? "WHERE {$where}" : "";
+        $sql = "DELETE FROM {$table} {$where}";
+        if ($this->query($sql) == TRUE)
+            return 1;
+
+        else {
+            echo "Lỗi: " . $this->conn->error;
+        }
+    }
 }
 
 $db = new DB;
 $data = array(
-    'username' => 'ngocha',
-    'password' => md5('Hoang1234')
+    'username' => 'Nguyễn Thế Hiệu',
+    'password' => md5('Hieu1!!@#'),
 );
-echo $db->insert('users', $data);
+// echo $db->insert('users', $data);
 // $db->connection();
+// $users = $db->update('users', $data, 'id=1');
+
+$db->delete('users', 'id=2');
+
+// echo "<pre>";
+// print_r($users);
+// echo "</pre>";
+
+
+// echo $users['username'];
